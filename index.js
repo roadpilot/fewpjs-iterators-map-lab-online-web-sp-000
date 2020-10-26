d6f5a7bd85fe00ae5459a34bbe2534940d49f999
mdacht777 <mdacht777@gmail.com>
2020-10-26 17:26:34 -0600

    commit tree 35ca9d55d45d567dff88fb9a946121a9046fa1f1

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (title) {
+    let words = title.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      words[i] = (words[i].charAt(0).toUpperCase() + words[i].slice(1));
+    }
+    return words.join(' ');
+  });
 }
